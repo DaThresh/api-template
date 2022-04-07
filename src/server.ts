826c2f8d58ac1318488ce38197d6e469c22561ff
Andrew Thresher <thresh3@me.com>
2022-04-07 12:15:32 -0500

    Updated server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,27 @@
-console.log('hello world');
+import dotenv from 'dotenv';
+dotenv.config();
+
+import ApiServer from './boundaries/api';
+import AppDB from './boundaries/database';
+import { MissingEnvironmentError } from './utilities/errors';
+
+if (
+  !process.env.databaseHost ||
+  !process.env.databaseName ||
+  !process.env.databaseUsername ||
+  !process.env.databasePassword ||
+  !process.env.databasePort
+) {
+  throw new MissingEnvironmentError('Database variables not present');
+}
+AppDB.connect({
+  host: String(process.env.databaseHost),
+  name: String(process.env.databaseName),
+  username: String(process.env.databaseUsername),
+  password: String(process.env.databasePassword),
+  port: Number(process.env.databasePort),
+});
+
+ApiServer.init();
+ApiServer.registerApiCatch();
+ApiServer.listen(8000);
