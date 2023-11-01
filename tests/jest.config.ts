1900f27d38a6ff90522c3cb407a481622a9ca8e8
Andrew Thresher <thresh3@me.com>
2023-11-01 18:47:27 -0500

    test: add coverage instructions

--- tests/jest.config.ts
+++ tests/jest.config.ts
@@ -3,8 +3,11 @@ import { Config } from 'jest';
 export const config: Config = {
   verbose: true,
   preset: 'ts-jest',
+  rootDir: '../',
   clearMocks: true,
   testEnvironment: 'node',
+  collectCoverageFrom: ['./src/*.ts', 'src/**/*.ts'],
+  coveragePathIgnorePatterns: ['./src/server.ts', './src/app.ts'],
 };
 
 export default config;
